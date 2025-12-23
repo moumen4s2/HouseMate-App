d553c3baec92bbc81edfb680bf950615ccec48c6
Omran abbas <omranabbas77@gmail.com>
2025-12-22 22:08:53 -0800

    edit AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -22,6 +22,7 @@ class AdminSeeder extends Seeder
         'phone'=>"0000000000",
         'password'=>Hash::make("0000000000"),
         'id_document_url'=>'profiles/default-profile.jpg',
+        'avatar_url'=>asset('storage/' . 'profiles/default-profile.jpg'),
         'is_approved'=>true,
         'phone_verified_at'=>now(),
         'date_of_birth'=>'2001-1-1'
